58f112978a564b20212a0ec000c9d171c65bb5a0
Bennett Wu <57691028+bwu1324@users.noreply.github.com>
2022-11-26 11:55:41 -0600

    fix: not convert message to buffer before comparison

--- src/processor/frame_processor.ts
+++ src/processor/frame_processor.ts
@@ -73,11 +73,11 @@ process.once("message", (message: string) => {
   InitProcessorss(settings);
 
   process.on("message", (message: Buffer) => {
+    message = Buffer.from(message);
     if (Buffer.compare(message, stop_sig)) {
       Stop();
       return;
     }
-    message = Buffer.from(message);
     const timestamp = Number(message.readBigUInt64BE(0));
     process_queue.push({ frame: message.subarray(8), timestamp });
     if (process_queue.length > 30) {
